09cb9b02e4d73fb3e4344cce596a3a4d3cba2c3d
TheCodingPantsula <asmdigital14@gmail.com>
2023-03-16 10:05:13 -0700

    advancded

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
-
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+*main - print the string in the put function
+*Return: 0
 */
-int main() {
-  printf("Size of char: %ld byte(s)\n", sizeof(char));
-  printf("Size of short: %ld byte(s)\n", sizeof(short));
-  printf("Size of int: %ld byte(s)\n", sizeof(int));
-  printf("Size of long: %ld byte(s)\n", sizeof(long));
-  printf("Size of long long: %ld byte(s)\n", sizeof(long long));
-  printf("Size of float: %ld byte(s)\n", sizeof(float));
-  printf("Size of double: %ld byte(s)\n", sizeof(double));
-  printf("Size of long double: %ld byte(s)\n", sizeof(long double));
-  printf("Size of void pointer: %ld byte(s)\n", sizeof(void *));
-  
-  return 0;
+int main(void)
+{
+printf("Size of char: %ld byte(s)\n", sizeof(char));
+printf("Size of short: %ld byte(s)\n", sizeof(short));
+printf("Size of int: %ld byte(s)\n", sizeof(int));
+printf("Size of long: %ld byte(s)\n", sizeof(long));
+printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+printf("Size of float: %ld byte(s)\n", sizeof(float));
+printf("Size of double: %ld byte(s)\n", sizeof(double));
+printf("Size of long double: %ld byte(s)\n", sizeof(long double));
+printf("Size of void pointer: %ld byte(s)\n", sizeof(void *));
+return (0);
 }
