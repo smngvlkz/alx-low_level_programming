373a179b90f58f24ef0e1586d4a0a4b0050e2bd8
TheCodingPantsula <asmdigital14@gmail.com>
2023-03-16 05:30:25 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,10 @@ int main(void)
 	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu  bytes(s)\n", sizeof(float));
-	return (0);
+	printf("The size of long long is %lu byte(s).\n", sizeof(long long));
+        printf("The size of float is %lu byte(s).\n", sizeof(float));
+        printf("The size of double is %lu byte(s).\n", sizeof(double));
+        printf("The size of long double is %lu byte(s).\n", sizeof(long double));
+        printf("The size of pointer is %lu byte(s).\n", sizeof(void*));
+        return (0);
 }
