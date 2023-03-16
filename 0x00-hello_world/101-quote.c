de4317d0019628f2a2a1ad9d5bfce44a8b812efa
TheCodingPantsula <asmdigital14@gmail.com>
2023-03-16 10:50:41 -0700

    last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-
-int main() {
-  const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, message, 50);
-  return 1;
+#include <string.h>
+/**
+ *main - Entry point
+ *
+ *Return: 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
