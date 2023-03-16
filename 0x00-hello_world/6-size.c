dcb6228322a97d98144dd50d1acbec6f2293f757
TheCodingPantsula <asmdigital14@gmail.com>
2023-03-16 12:14:15 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,15 @@
 */
 int main(void)
 {
-printf("Size of char: %d byte(s)\n", sizeof(char));
-printf("Size of short: %d byte(s)\n", sizeof(short));
-printf("Size of int: %d byte(s)\n", sizeof(int));
-printf("Size of long: %d byte(s)\n", sizeof(long));
-printf("Size of long long: %d byte(s)\n", sizeof(long long));
-printf("Size of float: %d byte(s)\n", sizeof(float));
-printf("Size of double: %d byte(s)\n", sizeof(double));
-printf("Size of long double: %d byte(s)\n", sizeof(long double));
-printf("Size of void pointer: %d byte(s)\n", sizeof(void *));
+char a;
+int b,
+long int c;
+long long int d;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
