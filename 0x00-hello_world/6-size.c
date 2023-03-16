db9760c06cd6cd6a74a9ac8758acdb73e38f8a8f
TheCodingPantsula <asmdigital14@gmail.com>
2023-03-16 09:15:47 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 /**
- *main - Entry point
+ *You should produce the exact same output as in the example
+ *Warnings are allowed
  *
  *return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu  bytes(s)\n", sizeof(float));
-	printf("The size of long long is %lu byte(s).\n", sizeof(long long));
-        printf("The size of float is %lu byte(s).\n", sizeof(float));
-        printf("The size of double is %lu byte(s).\n", sizeof(double));
-        printf("The size of long double is %lu byte(s).\n", sizeof(long double));
-        printf("The size of pointer is %lu byte(s).\n", sizeof(void*));
+	char c;
+	int i;
+	long int li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %ld bytes(s)\n", sizeof(i));
+	printf("size of a long int: %ld bytes(s)\n", sizeof(li));
+	printf("size of a long long int: %ld bytes(s)\n", sizeof(lli));
+        printf("size of float is %ld byte(s).\n", sizeof(f));
         return (0);
 }
