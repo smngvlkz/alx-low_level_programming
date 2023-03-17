4f7c71a17f1f2b42d94cd9e60a3ec42591642064
TheCodingPantsula <asmdigital14@gmail.com>
2023-03-17 02:18:44 -0700

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,21 +7,23 @@
  */
 int main(void)
 {
-	int n, m;
+	int n;
+	int m;
 
 	for (n = 48; n <= 56; n++)
 	{
-		if (m > n)
-		{
-			putchar(n);
-			putchar(m);
-			if (n != 56 || m != 57)
+		for (m = 49; m < 56; m++)
 			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
 				putchar(',');
 				putchar(' ');
+				}
 			}
-		}
 	}
 	putchar('\n');
 	return (0);
 }
+
