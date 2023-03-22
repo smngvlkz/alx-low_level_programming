e231500d071b052a6bc69fc6200494744e04c5b9
TheCodingPantsula <asmdigital14@gmail.com>
2023-03-22 05:21:26 -0700

    chmod

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -13,15 +13,15 @@ int main(void)
 	int day;
 	int year;
 
-        month = 2;
+	month = 2;
 	day = 29;
-        year = 2000;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
 	day = convert_day(month, day);
-	
-    	print_remaining_days(month, day, year);
+
+	print_remaining_days(month, day, year);
 
 	return (0);
 }
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -4,13 +4,13 @@
 * print_remaining_days - takes a date and prints how many ays are
 * left in the year, taking leap years int account
 * @month: month in number format
+* @day: day of month
 * @year: year
 * Return: void
 */
 
 void print_remaining_days(int month, int day, int year)
 {
-
 	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 	{
 		if (month > 2 && day >= 60)
