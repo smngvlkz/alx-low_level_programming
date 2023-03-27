31ed8ee24fe74fcb31e08b20c70e37687456c41d
TheCodingPantsula <asmdigital14@gmail.com>
2023-03-27 06:59:14 -0700

    Keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "main.h"
 
 /**
  * main - program that generates random valid
@@ -10,26 +11,17 @@
  */
 int main(void)
 {
-	int pass[100];
-	int i, sum, n;
+	char c;
+	int x;;;
 
-	sum = 0;
-
-	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
+	sran(time(0));
+	while (x <= 2645)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - sum - '0' < 78)
-		{
-			n = 2772 - sum - '0'';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		c = rand() % 128;
+		x += c;
+		putchar(c);
 	}
+	putchar(2772 - x);
 
 	return (0);
 }
