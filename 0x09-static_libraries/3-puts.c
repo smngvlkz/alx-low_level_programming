f06a4e328de25cfa512add8b446ba3b9e41c4555
TheCodingPantsula <asmdigital14@gmail.com>
2023-04-08 04:07:12 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,14 +2,14 @@
 
 /**
  * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * @s: string to print
  */
 
-void _puts(char *str)
+void _puts(char *s)
 {
-	while (*str != '\0')
+	while (*s != '\0')
 	{
-		_putchar(*str++);
+		_putchar(*s++);
 	}
 		_putchar('\n');
 }
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -3,4 +3,4 @@
 gcc -Wall -Werror -Wextra -pedantic -c *.c
 ar -rc liball.a *.o
 ranlib liball.a
-rm *.o
+
