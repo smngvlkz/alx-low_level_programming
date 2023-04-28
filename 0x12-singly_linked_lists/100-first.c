4d8ec7582cd8e57137e40a7c4f55d5d13b823ee5
TheCodingPantsula <asmdigital14@gmail.com>
2023-04-28 12:43:23 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void __attribute__ ((constructor)) beforemain()
+void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
