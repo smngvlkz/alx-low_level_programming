07da77caf943f353436be01584f3ee4a84da6a46
TheCodingPantsula <asmdigital14@gmail.com>
2023-04-28 13:43:44 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * marked with the attribute
+ * sentence will be executed before the main function
+ */
+
 void __attribute__ ((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n"\n);
 	printf("I bore my house upon my back!\n");
 }
