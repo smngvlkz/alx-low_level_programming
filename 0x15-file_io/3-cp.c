b7e3aa0ea1760474b361fa0a6f1ba609c92650ef
TheCodingPantsula <asmdigital14@gmail.com>
2024-02-09 13:16:47 +0200

    Revert "Delete 0x14-bit_manipulation directory"
    This reverts commit 28e7324f93a31286980f8f44dd43f2dccbf92c34.
    
    	new file:   0x14-bit_manipulation/.4-clear_bit.c.swp
    	new file:   0x14-bit_manipulation/.5-main.c.swp
    	new file:   0x14-bit_manipulation/0-binary_to_uint.c
    	new file:   0x14-bit_manipulation/0-main.c
    	new file:   0x14-bit_manipulation/1-main.c
    	new file:   0x14-bit_manipulation/1-print_binary.c
            new file:   0x14-bit_manipulation/100-get_endianness.c
    	new file:   0x14-bit_manipulation/100-main.c
    	new file:   0x14-bit_manipulation/101-password
    	new file:   0x14-bit_manipulation/2-get_bit.c
    	new file:   0x14-bit_manipulation/2-main.c
    	new file:   0x14-bit_manipulation/3-main.c
    	new file:   0x14-bit_manipulation/3-set_bit.c
    	new file:   0x14-bit_manipulation/4-clear_bit.c
    	new file:   0x14-bit_manipulation/4-main.c
    	new file:   0x14-bit_manipulation/5-flip_bits.c
    	new file:   0x14-bit_manipulation/5-main.c
    	new file:   0x14-bit_manipulation/README.md
    	new file:   0x14-bit_manipulation/_putchar.c
    	new file:   0x14-bit_manipulation/a
    	new file:   0x14-bit_manipulation/b
    	new file:   0x14-bit_manipulation/c
    	new file:   0x14-bit_manipulation/crackme3
    	new file:   0x14-bit_manipulation/d
    	new file:   0x14-bit_manipulation/e
    	new file:   0x14-bit_manipulation/f
    	new file:   0x14-bit_manipulation/h
    	new file:   0x14-bit_manipulation/main.h
    
    COMMINT
    :wq
    :x

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: the number of arguments
+ * @argv: the arguments vector
+ *
+ * Return: 0 on success, exit if it fails
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
+	ssize_t r_read, w_written;
+	char buffer[BUFFER_SIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+
+	while ((r_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w_written = write(file_to, buffer, r_read);
+		if (w_written == -1 || w_written != r_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from);
+			close(file_to);
+			exit(99);
+		}
+	}
+	if (r_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(file_from);
+		close(file_to);
+		exit(98);
+	}
+
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		close(file_to);
+		exit(100);
+	}
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
