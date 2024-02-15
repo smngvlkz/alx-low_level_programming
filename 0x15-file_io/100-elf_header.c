1931b55c8834153b8d26cd6e4dc0b1e832e6d26e
smngvlkz <asmdigital14@gmail.com>
2024-02-15 02:59:51 -0800

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -19,11 +19,10 @@ void print_entry(unsigned long int e_entity, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - Checks if a file is an ELF file.
+ * check_elf - Checks if a file is an ELF file
  * @e_ident: pointer to an array containing the ELF magic numbers
- *
  * Description: if the file is not an ELF file, exit code 98
- */
+ * */
 void check_elf(unsigned char *e_ident)
 {
 	int index;
