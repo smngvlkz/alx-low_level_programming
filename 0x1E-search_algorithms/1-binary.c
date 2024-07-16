cb5a8c91673e396ec3dd5651e04090d4a0649a61
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-16 18:52:19 +0200

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,47 +9,48 @@
  */
 void print_array(int *array, size_t left, size_t right)
 {
-    size_t i;
+	size_t i;
 
-    printf("Searching in array: ");
-    for (i = left; i <= right; i++)
-    {
-        printf("%d", array[i]);
-        if (i < right)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
 }
 
 /**
- * binary_search - Searches for a value in a sorted array using Binary search
- * @array: Pointer to the first element of the array to search in
+ * binary_search - Searches for a value in a sorted
+ * array using Binary search
+ * @array: Pointer to the first element of
+ * the array to search in
  * @size: Number of elements in the array
  * @value: Value to search for
- * Return: Index where value is located, or -1 if not found or array is NULL
+ * Return: Index where value is located,
+ * or -1 if not found or array is NULL
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (left <= right)
-    {
-        print_array(array, left, right);
+	while (left <= right)
+	{
+		print_array(array, left, right);
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
-        mid = (left + right) / 2;
-
-        if (array[mid] == value)
-            return (mid);
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-
-    return (-1);
+	return (-1);
 }
