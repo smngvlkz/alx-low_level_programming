2303476356495803a6bb6d61ca61e02db537230e
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-17 11:12:00 +0200

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -6,49 +6,53 @@
  * min - Returns the minimum of two size_t values
  * @a: First value
  * @b: Second value
+ *
  * Return: Minimum of a and b
  */
 size_t min(size_t a, size_t b)
 {
-    return (a < b) ? a : b;
+	return ((a < b) ? a : b);
 }
 
 /**
- * jump_search - Searches for a value in a sorted array using Jump search
+ * jump_search - Searches for a value in a
+ * sorted array using Jump search
  * @array: Pointer to the first element of the array to search in
  * @size: Number of elements in the array
  * @value: Value to search for
- * Return: First index where value is located, or -1 if not found or array is NULL
+ *
+ * Return: First index where value is located,
+ * or -1 if not found or array is NULL
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t step, prev, curr;
+	size_t step, prev, curr;
 
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    step = sqrt(size);
-    prev = 0;
-    curr = 0;
+	step = sqrt(size);
+	prev = 0;
+	curr = 0;
 
-    while (curr < size && array[curr] < value)
-    {
-        printf("Value checked array[%lu] = [%d]\n", curr, array[curr]);
-        prev = curr;
-        curr += step;
-    }
+	while (curr < size && array[curr] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", curr, array[curr]);
+		prev = curr;
+		curr += step;
+	}
 
-    printf("Value found between indexes [%lu] and [%lu]\n", prev, curr);
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, curr);
 
-    curr = min(curr, size - 1);
+	curr = min(curr, size - 1);
 
-    while (prev <= curr)
-    {
-        printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-        if (array[prev] == value)
-            return (prev);
-        prev++;
-    }
+	while (prev <= curr)
+	{
+		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		if (array[prev] == value)
+			return (prev);
+		prev++;
+	}
 
-    return (-1);
+	return (-1);
 }
